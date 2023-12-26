8cb6df96dee48f849699d20188c868ddc9028bde
yacinekedidi <kedidiyacine@gmail.com>
2023-12-26 05:46:23 +0100

    sql script added unique contraints

--- scripts/mobiphone2.sql
+++ scripts/mobiphone2.sql
@@ -7,7 +7,7 @@ USE mobiphone2;
 -- Création de la table "compte"
 CREATE TABLE IF NOT EXISTS compte (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    login VARCHAR(255) NOT NULL,
+    login VARCHAR(255) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL,
     role ENUM(
         'responsable commercial',
@@ -23,11 +23,11 @@ CREATE TABLE IF NOT EXISTS compte (
 -- Création de la table "client"
 CREATE TABLE IF NOT EXISTS client (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    cin VARCHAR(20) NOT NULL,
+    cin VARCHAR(20) NOT NULL UNIQUE,
     nom VARCHAR(255) NOT NULL,
     prenom VARCHAR(255) NOT NULL,
     adresse_de_livraison VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_maj TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     INDEX idx_cin (cin),
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS client (
 -- Création de la table "session"
 CREATE TABLE IF NOT EXISTS session (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    compte_id INT,
+    compte_id INT UNIQUE,
     date_debut TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (compte_id) REFERENCES compte(id),
     INDEX idx_compte_id (compte_id)
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS article (
 -- Création de la table "telephone_mobile"
 CREATE TABLE IF NOT EXISTS telephone_mobile (
     id_article INT PRIMARY KEY,
-    reference VARCHAR(255),
+    reference VARCHAR(255) UNIQUE,
     marque VARCHAR(255),
     modele VARCHAR(255),
     FOREIGN KEY (id_article) REFERENCES article(id)
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS telephone_mobile (
 -- Création de la table "ligne_telephonique"
 CREATE TABLE IF NOT EXISTS ligne_telephonique (
     id_article INT PRIMARY KEY,
-    numero VARCHAR(255),
+    numero VARCHAR(255) UNIQUE,
     operateur VARCHAR(255),
     montant_min_consommation DOUBLE,
     FOREIGN KEY (id_article) REFERENCES article(id)
@@ -75,7 +75,7 @@ CREATE TABLE IF NOT EXISTS ligne_telephonique (
 -- Création de la table "cle_3g"
 CREATE TABLE IF NOT EXISTS cle_3g (
     id_article INT PRIMARY KEY,
-    numero_serie VARCHAR(255),
+    numero_serie VARCHAR(255) UNIQUE,
     debit_connexion DOUBLE,
     capacite_max_telechargement DOUBLE,
     FOREIGN KEY (id_article) REFERENCES article(id)
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS cle_3g (
 -- Création de la table "carte_telephonique"
 CREATE TABLE IF NOT EXISTS carte_telephonique (
     id_article INT PRIMARY KEY,
-    code VARCHAR(255),
+    code VARCHAR(255) UNIQUE,
     duree_validite INT,
     type_carte VARCHAR(25),
     operateur VARCHAR(255),
